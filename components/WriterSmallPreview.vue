706252706b92b78c8d805611e3d4fdbb2ccbf89d
FeatheredSnek <dorian.maczka@gmail.com>
2022-05-11 11:21:24 +0200

    conformed to simplified writer api

--- components/WriterSmallPreview.vue
+++ components/WriterSmallPreview.vue
@@ -45,10 +45,8 @@
       }
     },
     async fetch() {
-      // let apiURL = `${this.$config.apiBaseURL}/writer.php?friendly_id=${this.$props.id}`;
-      let apiURL = `${this.$config.apiBaseURL}/writer.php?friendly_id=john_cook`;
-      let apiResponseA = await this.$http.$get(apiURL);
-      let apiResponse = apiResponseA.writer
+      let apiURL = `${this.$config.apiBaseURL}/writer.php?friendly_id=${this.$props.id}`;
+      let apiResponse = await this.$http.$get(apiURL);
       this.name = apiResponse.name
       this.shortBio = apiResponse.short_bio
       this.socials = apiResponse.socials
