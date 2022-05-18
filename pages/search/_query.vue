7c8f933c255d6274285b355e6462f7a1aec06960
FeatheredSnek <dorian.maczka@gmail.com>
2022-05-18 23:23:53 +0200

    search styling + search method with route query

--- pages/search/_query.vue
+++ pages/search/_query.vue
@@ -1,137 +1,211 @@
 <template>
-  <div>
-    <div 
-      v-if="$fetchState.pending"
-      class="loading"
-    >
-      Loading data
-    </div>
-    <div 
-      v-else-if="$fetchState.error"
-      class="error"
-    >
-      Data loading error
-    </div>
-    <div v-else>
-      <div>
-        <h2>ELELELELAdvanced search parameters = {{ debugIsPiesUrl }}</h2>
-        <div>
-          <label for="query"></label>
+  <div class="flex w-full gap-8 lg:gap-16 flex-col sm:flex-row">
+    
+    <!-- responsive advanced search form -->
+    <div class="sm:w-1/3 lg:w-1/3 w-full">
+      <form 
+      @submit.prevent="advancedSearch()"
+      class="p-6 block w-full h-fit bg-white rounded-xl overflow-hidden border border-gray-200"
+      >
+        
+        <p class="text-2xl mb-3 font-bold text-gray-600">
+          Advanced search
+        </p>
+        
+        <div class="mb-3">
+          <label for="query" class="text-lg lg:text-xl">Search for</label>
           <input 
             type="text" 
             id="query" 
             v-model="params.query"
+            class="w-full h-12 py-1 pl-4 pr-8 border-gray-300 focus:ring-lime-500 focus:border-lime-500 rounded-lg"
+            placeholder="e.g. caesar"
           />
         </div>
-        <div>
-          <label for="vegan">vegan</label>
-          <input 
-            type="checkbox"
-            id="vegan"
-            v-model="params.vegan"
-          />
-        </div>
-        <div>
-          <label for="vegetarian">vegetarian</label>
-          <input 
-            type="checkbox"
-            id="vegetarian"
-            v-model="params.vegetarian"
-          />
-        </div>
-        <div>
-          <label for="maindish">maindish</label>
-          <input 
-            type="checkbox"
-            id="maindish"
-            v-model="params.maindish"
-          />
-        </div>
-        <div>
-          <label for="hot">hot</label>
-          <input 
-            type="checkbox"
-            id="hot"
-            v-model="params.hot"
-          />
-        </div>
-        <div>
-          <label for="time">max prep time {{ params.time }}</label>
+
+        <ul class="mb-3">
+          <li class="text-lg lg:text-xl mb-1 lg:mb-2 flex items-center">
+            <input 
+              type="checkbox" 
+              id="vegan"
+              v-model="params.vegan"
+              class="w-4 h-4 mr-1 lg:w-5 lg:h-5 lg:mr-2 "
+            />
+            <label for="vegan">Vegan</label>
+          </li>
+          <li class="text-lg lg:text-xl mb-1 lg:mb-2 flex items-center">
+            <input 
+              type="checkbox" 
+              id="vegetarian"
+              v-model="params.vegetarian"
+              class="w-4 h-4 mr-1 lg:w-5 lg:h-5 lg:mr-2 "
+            />
+            <label for="vegetarian">Vegetarian</label>
+          </li>
+          <li class="text-lg lg:text-xl mb-1 lg:mb-2 flex items-center">
+            <input 
+              type="checkbox" 
+              id="maindish"
+              v-model="params.maindish"
+              class="w-4 h-4 mr-1 lg:w-5 lg:h-5 lg:mr-2 "
+            />
+            <label for="maindish">Main dish candidate</label>
+          </li>
+          <li class="text-lg lg:text-xl mb-1 lg:mb-2 flex items-center">
+            <input 
+              type="checkbox" 
+              id="hot"
+              v-model="params.hot"
+              class="w-4 h-4 mr-1 lg:w-5 lg:h-5 lg:mr-2 "
+            />
+            <label for="hot">Served hot</label>
+          </li>
+        </ul>
+
+        <!-- prep time slider -->
+        <div class="mb-3">
+          <label for="time" class="text-lg lg:text-xl">
+            Prep time 
+            <span :class="timeSliderLabel.css">
+              {{ timeSliderLabel.text }}
+            </span>
+          </label>
+          <!-- no v-model here because i need to manually do the first data update -->
           <input 
             type="range"
             id="time"
             min="10"
             max="30"
             step="5"
-            v-model="params.time"
+            @input="getTimeSliderValue($event.target.value)"
+            :value="updateTimeSliderValue()"
           />
         </div>
-        <div>
-          <button @click="advancedSearch()">
-            Advanced search
-          </button>
-        </div>
+
+        <AppActionButton>
+          <template #icon>
+            <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+          </template>
+          <template #text>
+            Search
+          </template>
+        </AppActionButton>
+
+      </form>
+    </div>
+
+    <!-- search results -->
+    <div class="sm:w-2/3 lg:w-2/3 w-full">
+      <div v-if="$fetchState.pending"
+        class="loading"
+      >
+        Loading data
       </div>
-      <h1>Search results</h1>
-      <div v-if="!results.length">
+      <div v-else-if="$fetchState.error"
+        class="error"
+      >
+        Data loading error
+      </div>
+      <div v-else-if="!results.length">
         No results found!
       </div>
-      <RecipeSmallPreview
-        v-else
-        v-for="recipe in results"
-        :key="recipe.friendlyId"
-        :name="recipe.name"
-        :blurb="recipe.blurb"
-        :friendly-id="recipe.friendly_id"
-      />
+      <div v-else>
+        <h2 class="font-bold text-3xl text-gray-600 pb-4">
+          Search results
+        </h2>
+        <RecipeBigPreview
+          v-for="recipe in results"
+          :key="recipe.friendlyId"
+          :name="recipe.name"
+          :blurb="recipe.blurb"
+          :friendly-id="recipe.friendly_id"
+          :image="recipe.image"
+          :flags="recipe.flags"
+          :writer="recipe.writer"
+          class="mb-8"
+        />
+      </div>
     </div>
+
   </div>
 </template>
 
 <script>
-import RecipeSmallPreview from "@/components/RecipeSmallPreview.vue"
+import RecipeBigPreview from "@/components/RecipeBigPreview.vue"
+import AppActionButton from "@/components/AppActionButton.vue"
 
 export default {
   name: 'SearchResultsPage',
   components: { 
-    RecipeSmallPreview
+    RecipeBigPreview,
+    AppActionButton
   },
   watch: {
-    '$route.params.id': '$fetch'
+    '$route.params': '$fetch',
+    '$route.query': '$fetch'
   },
   data() {
     return {
-      debugIsPiesUrl: 'default',
       results: [],
-      advanced: false,
       params: {
         query: '',
         vegan: false,
         vegetarian: false,
         maindish: false,
         hot: false,
-        time: 0
+        time: null
       }
     }
   },
   async fetch() {
-    let apiURL = `${this.$config.apiBaseURL}/search.php?q=${this.$route.params.query}`;
-    if (this.advanced) {
-      let vegan = this.params.vegan ? '&vegan=1' : ''
-      let vegetarian = this.params.vegetarian ? '&vegetarian=1' : ''
-      let maindish = this.params.maindish ? '&maindish=1' : ''
-      let hot = this.params.hot ? '&hot=1' : ''
-      let time = this.params.time ? '&preptime=' + this.params.time : ''
-      apiURL = `${this.$config.apiBaseURL}/search.php?q=${this.$route.params.query}${vegan}${vegetarian}${maindish}${hot}${time}`
-    }
-    this.debugIsPies = apiURL
+    let apiURL = `${this.$config.apiBaseURL}/search.php?q=${this.$route.params.query}`
+    if (this.$route.query.vegan == true) apiURL += '&vegan=1'
+    if (this.$route.query.vegetarian == true) apiURL += '&vegetarian=1'
+    if (this.$route.query.maindish == true) apiURL += '&maindish=1'
+    if (this.$route.query.hot == true) apiURL += '&hot=1'
+    if (this.$route.query.time) apiURL += `&preptime=${this.$route.query.time}`
     let apiResponse = await this.$http.$get(apiURL);
     this.results = apiResponse
   },
   methods: {
     advancedSearch() {
-      this.advanced = true
-      this.$fetch()
+      const query = {}
+      if (this.params.vegan) query.vegan = true
+      if (this.params.vegetarian) query.vegetarian = true
+      if (this.params.maindish) query.maindish = true
+      if (this.params.hot) query.hot = true
+      if (this.params.time) query.time = this.params.time
+      this.$router.push(
+        { 
+          name: 'search-query', 
+          params: { query: this.params.query },
+          query
+        }
+      )
+    },
+    getTimeSliderValue(val) {
+      this.params.time = val
+    },
+    updateTimeSliderValue() {
+      if (this.params.time === null) {
+        return 20
+      }
+      else {
+        return this.params.time
+      }
+    }
+  },
+  computed: {
+    timeSliderLabel() {
+      const obj = {
+        text: 'not specified',
+        css: ''
+      }
+      if (this.params.time !== null) {
+        obj.text = `up to ${this.params.time} minutes`
+        obj.css = 'font-bold'
+      }
+      return obj
     }
   }
 }
