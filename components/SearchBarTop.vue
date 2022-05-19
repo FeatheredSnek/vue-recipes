a415fb1310254c3bf649c23a4e954c054109ca1c
FeatheredSnek <dorian.maczka@gmail.com>
2022-05-19 13:06:06 +0200

    rudimentary query validation

--- components/SearchBarTop.vue
+++ components/SearchBarTop.vue
@@ -9,7 +9,7 @@
         v-model="query"
       >
       <button class="absolute inset-y-0 w-11 right-0 flex items-center justify-center rounded-lg focus:outline-none focus:ring-2 focus:ring-inset focus:ring-lime-500">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" :class="{'bounce-active': bouncingButton}" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
           <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
         </svg>
       </button>
@@ -23,7 +23,8 @@
     name: "SearchBarTop",
     data() {
       return {
-        query: ''
+        query: '',
+        bouncingButton: false
       }
     },
     watch: {
@@ -41,7 +42,17 @@
     },
     methods: {
       search() {
-        this.$router.push({ name: 'search-query', params: { query: this.query } })
+        const regex = /[A-Za-z0-9]/
+        const minlength = 2
+        if (this.query.match(regex) && this.query.length >= minlength) {
+          this.$router.push({ name: 'search-query', params: { query: this.query } })
+        }
+        else {
+          this.bouncingButton = true
+          setTimeout(() => {
+            this.bouncingButton = false
+          }, 550);
+        }
       }
     },
     
@@ -50,4 +61,28 @@
 
 <style scoped>
 
+.bounce-active {
+  animation: bounce 0.25s;
+  animation-iteration-count: 2;
+  animation-timing-function: ease-in-out;
+}
+
+.bounce-inactive {
+  animation: none;
+}
+
+@keyframes bounce {
+  0% {
+    transform: rotate(-15deg);
+  }
+  33% {
+    transform: rotate(0deg);
+  }
+  66% {
+    transform: rotate(15deg);
+  }
+  100% {
+    transform: rotate(0deg);
+  }
+}
 </style>
