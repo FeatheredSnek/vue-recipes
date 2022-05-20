23a27a25c66c1fd91d0ebfb05782c123392bb7ef
FeatheredSnek <dorian.maczka@gmail.com>
2022-05-20 22:15:20 +0200

    disabled button state prop & classes

--- components/AppActionButton.vue
+++ components/AppActionButton.vue
@@ -1,7 +1,9 @@
 <template>
   <button 
     @click="$emit('click')"
-    class="hover:bg-gray-600 text-gray-600 hover:text-gray-100 font-bold w-full h-12 rounded-lg flex items-center justify-center border border-gray-300 hover:border-0"
+    :disabled="buttonDisabled"
+    class="font-bold w-full h-12 rounded-lg flex items-center justify-center border border-gray-300 "
+    :class="clickable"
   >
     <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
       <slot name="icon"></slot>
@@ -13,6 +15,22 @@
 <script>
 export default {
   name: "AppActionButton",
-  emits: ['click']
+  emits: ['click'],
+  props: {
+    buttonDisabled: {
+      type: Boolean,
+      required: false
+    }
+  },
+  computed: {
+    clickable() {
+      if (!this.buttonDisabled) {
+        return 'hover:bg-gray-600 text-gray-600 hover:text-gray-100 hover:border-0'
+      }
+      else {
+        return 'text-gray-300'
+      }
+    }
+  }
 }
 </script>
