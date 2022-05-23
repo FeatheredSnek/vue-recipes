5560d81a36cf77b55676b628eb1bf1f7f771c132
FeatheredSnek <dorian.maczka@gmail.com>
2022-05-23 13:31:37 +0200

    removed page specific metas

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,30 +11,17 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      // { 
-      //   hid: 'description', 
-      //   name: 'description', 
-      //   content: 'A perfect place to find delicious and healthy salad recipes, curated and taste-tested by passionate professionals.' 
-      // },
       { name: 'format-detection', content: 'telephone=no' },
       { 'http-equiv': "X-UA-Compatible", content: "IE=edge" },
       { 'http-equiv': "content-language", content: "en-US" },
             // open graph
       { property: 'og:site_name', content: 'nuxtSalad' },
-      // { property: 'og:title', content: 'nuxtSalad - delicious and healthy salad recipes' },
-      // { property: 'og:description', content: 'A perfect place to find delicious and healthy salad recipes, curated and taste-tested by passionate professionals.' },
       { property: 'og:type', content: 'website' }, 
-      // { property: 'og:url', content: process.env.BASE_URL || 'http://localhost:3000' }, // eslint-disable-line
-      // { property: 'og:image', content: '/static/images/recipes/9a9f4eb1ea8c.jpg' },
-      // { property: 'og:image:alt', content: 'A delicious salad you can make with one of our recipes' },
             // twitter cards
       { property: 'twitter:card', content: 'summary_large_image' },
       { property: 'twitter:creator', content: '@nuxtsalad' },
-      // { property: 'twitter:description', content: 'A perfect place to find delicious and healthy salad recipes, curated and taste-tested by passionate professionals.' },
       { property: 'twitter:domain', content: process.env.BASE_URL || 'http://localhost:3000' }, // eslint-disable-line
-      // { property: 'twitter:image', content: `${process.env.CDN_URL}/recipes/9a9f4eb1ea8c.jpg` }, // eslint-disable-line
       { property: 'twitter:site', content: '@nuxtsalad' },
-      // { property: 'twitter:title', content: 'nuxtSalad - delicious and healthy salad recipes' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
